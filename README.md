40674ec42b90ed6bf28366a14aa1108dbc92e888
wjy <wjy@ws-mp.fios-router.home>
2019-11-03 17:20:15 -0500

    哈哈哈哈、

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 撒比老苏
+
+
+magnet:?xt=urn:btih:
+b566b67f0e0372b6ca4580a7fe93505b4af05339
+
--- pro.py
+++ pro.py
@@ -1,7 +1,19 @@
 import numpy as np
 import pandas as pd
 from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
 
 f=pd.read_csv("BCHI-dataset_2019-03-04.csv")
+
+f=f[f.Indicator.isin(['Lung Cancer Mortality Rate (Age-Adjusted; Per 100,000 people)'])]
+tset=f[["Year","Sex","Race/Ethnicity","Place"]].values
+head=list(f.columns.values)
+resualt=f["Value"].values
+
+le = preprocessing.LabelEncoder()
+for i in range(4):
+    tset[:,i] = le.fit_transform(tset[:,i])
+
+X_train, X_test, y_train, y_test = train_test_split(tset, resualt, test_size=0.30)
 
 
