4c8f852e7f823de8d8f15fb822003cb0df40bff1
wjy <37wjym@gmail.com>
2019-12-02 19:03:09 -0500

    casnkllkdns

--- r.py
+++ r.py
@@ -10,8 +10,17 @@ data[:,0]=data[:,0]-2009
 resualt=f[:,0]
 resualt=np.reshape(resualt,(resualt.size,1))
 
+#线性回归
+
 lr=NN([[5],[1,'relu']])
 lr.fit(data,resualt,epochs=300)
-res=lr.predict(data)
+res1=lr.predict(data)
 
 print (res)
+
+
+#ann
+
+ann=NN([[5],[7,'sigmoid'],[3,'relu'],[1,'relu']])
+ann.fit(data,resualt,epoch=300)
+res2=lr.predict(data)
